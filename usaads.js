d1c9830970baf1662d1b6b65bff0ba809e971114
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2019-09-13 09:14:01 +0100

    Update usaads.js

--- usaads.js
+++ usaads.js
@@ -1,6 +1,1 @@
-<script src='//tags.clickintext.net/JkRROFjzvy48S' title='Footer Expand' type='text/javascript'/>
-<script src='//tags.clickintext.net/Jk5gFPe4ifG7v' title='Slide In' type='text/javascript'/>
-<script type='text/javascript'>
-/* <![CDATA[ */
 (function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.src="//wi8ckyov4p.s.ad6media.fr/?d="+(new Date).getTime()+"&r=";try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript";a.async=!0;b.parentNode.insertBefore(a,b)})();
-/* ]]> */</script> 
