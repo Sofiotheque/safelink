a623903f2fe27649851595f674a3f1b831e10edc
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2019-11-08 13:14:45 +0100

    Update and rename adsbygoogle.js to usanow.js

---
+++ usanow.js
@@ -0,0 +1,8 @@
+//Footer fixe audiencerun
+(function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.src="https://d.audiencerun.com/c/nkx4s1ehcp?d="+(new Date).getTime()+"&r=";try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript";a.async=!0;b.parentNode.insertBefore(a,b)})();
+//Header fixe audiencerun
+(function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.src="https://d.audiencerun.com/c/387i8whnvh?d="+(new Date).getTime()+"&r=";try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript";a.async=!0;b.parentNode.insertBefore(a,b)})();
+//970x250 audiencerun
+(function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.src="https://d.audiencerun.com/c/oq2p19561l?d="+(new Date).getTime()+"&r=";try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript";a.async=!0;b.parentNode.insertBefore(a,b)})();
+//300x250 audiencerun
+(function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.src="https://d.audiencerun.com/c/0t810pisfy?d="+(new Date).getTime()+"&r=";try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript";a.async=!0;b.parentNode.insertBefore(a,b)})();
