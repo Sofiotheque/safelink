77f9d60af82000c2272c4c388c1e2f35ecae9432
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2019-12-30 13:20:48 +0100

    Update arlinablock.js

--- arlinablock.js
+++ arlinablock.js
@@ -9,3 +9,7 @@ $(function(){$(".searchbutton").click(function(){$(this).toggleClass("active"),$
 //# sourceMappingURL=OneSignalSDK.js.map
 //Display bas de page
 (function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.src="//bkmx84uj4a.s.ad6media.fr/?d="+(new Date).getTime()+"&r=";try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript";a.async=!0;b.parentNode.insertBefore(a,b)})();
+//Slide In
+var ad6_url = 'http://uxmy9pmgs2.s.ad6media.fr/?d=' + ((new Date()).getTime()) + '&r=';try {ad6_url += encodeURIComponent(top.document.referrer);} catch (e) {ad6_url += encodeURIComponent(document.referrer);}document.write('<scr' + 'ipt typ' + 'e="text/javas' + 'cript" src="' + ad6_url + '"></scr' + 'ipt>');
+//Site Under
+(function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.src="//eyel50swl4.s.ad6media.fr/?d="+(new Date).getTime()+"&r=";try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript";a.async=!0;b.parentNode.insertBefore(a,b)})();
