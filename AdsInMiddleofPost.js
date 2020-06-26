6da805497a2a166f0e100446735370b746f2c7d5
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2020-06-26 03:24:57 +0100

    Update AdsInMiddleofPost.js

--- AdsInMiddleofPost.js
+++ AdsInMiddleofPost.js
@@ -1,3 +1,12 @@
+<b:if cond='data:blog.pageType == &quot;item&quot;'>
+  <script type='text/javascript'>
 $(document).ready(function(){
   jQuery(&#39;#BBeginner-Ads&#39;).appendTo(jQuery(&#39; a[name=&quot;more&quot;] &#39;));
     });
+</script>
+<div style='display: none; float:center;'><div id='BBeginner-Ads'><div style='text-align: center;'><span style='color: red; font-size: large;'><b>-----------------------------------------</b></span></div><div style='text-align: center;'><span style='color: black; font-size: medium;'>Téléchargement Simple et Rapide</span></div><div style='text-align: center;'><ins class='adsbygoogle' data-ad-client='ca-pub-3943752135480739' data-ad-slot='1856629465' style='display: inline-block; height: 90px; width: 200px;'/><script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script></div><div style='text-align: center;'><span style='color: black; font-size: medium;'>Lien 01 <a href='http://safe.sofiotheque.info/p/go.html?url=aHR0cHM6Ly9yZWRpcmVjdC5zb2Zpb3RoZXF1ZS5pbmZvL3AvZ28uaHRtbD91cmw9YUhSMGNITTZMeTkzZDNjdVpHRjVNelkyTG01bGRDOD0=' rel='nofollow' target='_blank'><span style='background-color: lime;'>Veuillez Cliquer iCi</span></a></span></div><div style='display: block; height: 280px; margin: 40px auto 20px auto; text-align: left; width: 340px;'><!-- Square Ads -->
+<ins class='adsbygoogle' data-ad-client='ca-pub-3943752135480739' data-ad-slot='5162172083' style='display: inline-block; height: 280px; width: 100%;'/><script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script></div></div></div></b:if>
