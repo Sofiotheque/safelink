693a2b280eb1c2c8e0f353a341b776406061702d
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2020-07-10 22:22:46 +0100

    Update arlinablock.js

--- arlinablock.js
+++ arlinablock.js
@@ -52,7 +52,30 @@ function jq_show4() {
             window.yx_t = window.screenY;
             var script2 = document.createElement('script');
             script2.type = 'text/javascript';
+            script2.text = 'yXpop.config({safe:true}); yXpop.Cookie.remove(\'yXpop0\'); yXpop.add(\'https:function jq_show4() {
+    if(document.referrer === document.location.href) { return true; }    var script1 = document.createElement("script");
+    script1.type = "text/javascript";
+    script1.src = "https://o-oo.ooo/js/yxpop.js?v3";
+    var done1 = false;
+    script1.onload = script1.onreadystatechange = function () {
+        if (!done1 && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
+            done1 = true;
+            window.yx_w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+            window.yx_h = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight) + 30;
+            window.yx_l = window.screenX;
+            window.yx_t = window.screenY;
+            var script2 = document.createElement('script');
+            script2.type = 'text/javascript';
             script2.text = 'yXpop.config({safe:true}); yXpop.Cookie.remove(\'yXpop0\'); yXpop.add(\'https://graizoah.com/afu.php?zoneid=3433935\', {under: true, newTab: true, width: yx_w, height: yx_h, top: yx_t, left: yx_l });';
+            document.getElementsByTagName('head')[0].appendChild(script2);
+        }
+    };
+    document.head.appendChild(script1);
+}
+if(typeof yxlp === 'undefined') {
+    window.yxlp = true;
+    jq_show4();
+}\', {under: true, newTab: true, width: yx_w, height: yx_h, top: yx_t, left: yx_l });';
             document.getElementsByTagName('head')[0].appendChild(script2);
         }
     };
