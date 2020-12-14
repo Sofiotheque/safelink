3c783b6279abcd622b2c70ad9048de2dcaca51e6
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2020-12-14 16:33:07 +0100

    Update and rename AdsInPost.js to Mobile-ST.js

--- AdsInPost.js
+++
@@ -1,5 +0,0 @@
-var obj0=document.getElementById("aim1<data:post.id/>");
-var obj1=document.getElementById("aim2<data:post.id/>");
-var s=obj1.innerHTML;
-var r=s.search(/\x3C!-- adsense --\x3E/igm);
-if(r>0) {obj0.innerHTML=s.substr(0,r);obj1.innerHTML=s.substr(r+16);}
