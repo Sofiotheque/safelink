016a16610da878c401faf63365c7516c0554a32c
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2020-12-27 00:56:10 +0100

    Update and rename Disqus.js to Comments.js

---
+++ Comments.js
@@ -0,0 +1,4 @@
+// Disqus
+!function f(){var b=document.createElement("script");b.src="https://sofiotheque-2.disqus.com/embed.js";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
+// Facebook
+!function f(){var b=document.createElement("script");b.src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v9.0&appId=1148902115190529&autoLogAppEvents=1";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
