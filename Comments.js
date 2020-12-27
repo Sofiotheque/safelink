346a4ae27195e6376f038510fc8520cce5c7ad8a
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2020-12-27 01:29:25 +0100

    Update Comments.js

--- Comments.js
+++ Comments.js
@@ -1,4 +1,18 @@
 // Disqus
 !function f(){var b=document.createElement("script");b.src="https://sofiotheque-2.disqus.com/embed.js";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
 // Facebook
-!function f(){var b=document.createElement("script");b.src="https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js#xfbml=1&version=v9.0&appId=1148902115190529&autoLogAppEvents=1";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
+!function f(){var b=document.createElement("script");b.src="";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js#xfbml=1&version=v9.0&appId=1148902115190529&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
