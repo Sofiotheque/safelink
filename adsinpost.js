dbb6830efd13e87f813c1746773f3968a44a9d15
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2020-12-29 19:11:33 +0100

    Update adsinpost.js

--- adsinpost.js
+++ adsinpost.js
@@ -1,7 +1,1 @@
-!function f(){
-  var obj0=document.getElementById("aim1<data:post.id/>");
-  var obj1=document.getElementById("aim2<data:post.id/>");
-  var s=obj1.innerHTML;
-  var r=s.search(/\x3C!--adsense--\x3E/igm);
-  if(r&gt;0) {obj0.innerHTML=s.substr(0,r);obj1.innerHTML=s.substr(r+14);}
-}();
+!function f(){var obj0=document.getElementById("aim1<data:post.id/>");var obj1=document.getElementById("aim2<data:post.id/>");var s=obj1.innerHTML;var r=s.search(/\x3C!--adsense--\x3E/igm);if(r&gt;0){obj0.innerHTML=s.substr(0,r);obj1.innerHTML=s.substr(r+14);}}();
