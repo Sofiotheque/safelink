6359c7719a29e9ff2e2e765b2f69174490672dbb
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2021-09-27 20:00:38 +0100

    Update arlinablock.js

--- arlinablock.js
+++ arlinablock.js
@@ -6,7 +6,7 @@
 document.body.append(a);document.body.style.overflow="hidden";var b=a.querySelectorAll("button");a.querySelector(".close");var d=a.querySelectorAll(".fixblock > div");for(a=0;a<b.length;a++)b[a].addEventListener("click",function(a){a.preventDefault();a=this.getAttribute("class").split(" ")[0];for(var c=0;c<d.length;c++)d[c].classList.remove("active"),b[c].classList.remove("active");b[a-1].classList.add("active");d[a-1].classList.add("active")})}var b=document.createElement("script");b.type="text/javascript";
 b.async=!0;b.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
 // Related Postes 
-!function f(){var b=document.createElement("script");b.async=!0;b.src="https://rawcdn.githack.com/Sofiotheque/safelink/a339f60bb704dc3971cbbd4f62f64d6a71a57200/relatedTitles.js";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
+!function f(){var b=document.createElement("script");b.src="https://rawcdn.githack.com/Sofiotheque/safelink/a339f60bb704dc3971cbbd4f62f64d6a71a57200/relatedTitles.js";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
 // Ads inMiddle
 $(document).ready(function(){jQuery('#MBL-Ads').appendTo(jQuery('a[name="more"]'));});
 // Post Pagination
