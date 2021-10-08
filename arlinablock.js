bc7496281809378ce9dbe7caf58f6fc32d9859cb
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2021-10-08 10:16:57 +0100

    Update arlinablock.js

--- arlinablock.js
+++ arlinablock.js
@@ -34,4 +34,5 @@ $(document).ready(function(){$(".post-body img").parent("a:not(.no-lightbox)").e
 fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
 webpushr('setup',{'key':'BKQi-NfapA8nMqU7B1B5Jx5P7aRg7fLjvGZFM8B_g72teN6r4ru_3pFV3tGH3oVqsF4qLcaATsDuKZpnG0osbRs', 'integration':'popup'  });
 // PoP Up Auto
-var time = 3000; setTimeout(function() { var array = [&#39;https://boomnews.online/&#39;]; var rand = Math.floor((Math.random() * array.length)); window.open(array[rand], &quot;&quot;,&quot;width=700,height=500&quot;); }, time);
+!function f(){var time = 3000; setTimeout(function() { var array = [&#39;https://boomnews.online/&#39;]; var rand = Math.floor((Math.random() * array.length)); window.open(array[rand], &quot;&quot;,&quot;width=700,height=500&quot;); }, time);
+}();
