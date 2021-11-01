e290f1fd1d2a4adedad74ebbc59c3ff34808da0b
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2021-11-01 21:10:57 +0100

    Update and rename SofioCSS.js to ALL1CSS.js

---
+++ ALL1CSS.js
@@ -0,0 +1,10 @@
+function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+loadCSS("");
+loadCSS("");
+loadCSS("");
+loadCSS("");
+
+!function f(){var b=document.createElement("link");b.async=!0;b.src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
+!function f(){var b=document.createElement("link");b.async=!0;b.src="https://cdn.jsdelivr.net/gh/Sofiotheque/safelink@master/fontFiraSans.css";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
+!function f(){var b=document.createElement("link");b.async=!0;b.src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
+!function f(){var b=document.createElement("link");b.async=!0;b.src="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
