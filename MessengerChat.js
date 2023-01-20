7733107d9ec8bd46d9cc7f537a61256020dd48fa
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2023-01-20 10:46:12 +0100

    Create MessengerChat.js

---
+++ MessengerChat.js
@@ -0,0 +1,19 @@
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "521016984705335");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v15.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    
