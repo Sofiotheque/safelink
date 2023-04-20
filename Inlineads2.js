0da4ca0705931b2f849ad3e18a2db134c707c171
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2023-04-20 04:36:33 +0100

    Update and rename Inlineads1.js to Inlineads2.js

--- Inlineads2.js
+++ Inlineads2.js
@@ -1,3 +1,5 @@
+for (var ads=document.querySelectorAll('ins.adsbygoogle'),i=0;i<ads.length;i++){if(ads[i]){(adsbygoogle = window.adsbygoogle || []).push({});}};
+function twtparalax(Ad1) {let paralax = document.getElementsByClassName ('twtparalax')[0]; if (paralax) {let Adsp = document.querySelectorAll ('.post-body p')[Ad1 - 1]; Adsp.parentNode .insertBefore (paralax, Adsp.nextSibling)}} twtparalax(2);
 $(document).ready(function(){jQuery('#MBL-Ads').appendTo(jQuery('a[name="more"]'));}); 
 document.querySelectorAll(".post-body Note")[0]?document.querySelectorAll(".post-body Note")[0].after(document.querySelector("div#HTML317 .HTML")):document.querySelector("div#HTML317 .HTML").innerHTML=""; 
 document.querySelectorAll(".post-body eBook")[0]?document.querySelectorAll(".post-body eBook")[0].after(document.querySelector("div#HTML318 .HTML")):document.querySelector("div#HTML318 .HTML").innerHTML=""; 
