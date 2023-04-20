2a7283dc40fa2aa627408762207a82d6787a8d6a
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2023-04-20 20:39:59 +0100

    Update and rename Inlineads7.js to Inlineads8.js

--- Inlineads8.js
+++ Inlineads8.js
@@ -1,5 +1,4 @@
-if(document.querySelectorAll(".post-body p")[1]){document.querySelectorAll(".post-body p")[1].after(document.querySelector("div#HTML399 .HTML"))}else if(document.querySelectorAll(".post-body br")[1]){document.querySelectorAll(".post-body br")[1].after(document.querySelector("div#HTML399 .HTML"))}else{document.querySelector("div#HTML399 .HTML").innerHTML="";}
-
+$(document).ready(function(){jQuery('#HTML399').appendTo(jQuery('.post-body p')[1]);});
 $(document).ready(function(){jQuery('#MBL-Ads').appendTo(jQuery('a[name="more"]'));});
 $(document).ready(function(){jQuery('#HTML339').appendTo(jQuery('.post-body LessonNote'));});
 $(document).ready(function(){jQuery('#HTML335').appendTo(jQuery('.post-body MoreLink'));});
