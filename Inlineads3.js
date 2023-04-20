a6563f5e3288bf9e369afffad05c248834d99024
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2023-04-20 18:06:26 +0100

    Update and rename Inlineads3.js to Inlineads5.js

--- Inlineads3.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){jQuery('#MBL-Ads').appendTo(jQuery('a[name="more"]'));}); 
-document.querySelectorAll(".post-body Note")[0]?document.querySelectorAll(".post-body Note")[0].after(document.querySelector("div#HTML317 .HTML")):document.querySelector("div#HTML317 .HTML").innerHTML=""; 
-document.querySelectorAll(".post-body eBook")[0]?document.querySelectorAll(".post-body eBook")[0].after(document.querySelector("div#HTML318 .HTML")):document.querySelector("div#HTML318 .HTML").innerHTML=""; 
-document.querySelectorAll(".post-body LessonNote")[0]?document.querySelectorAll(".post-body LessonNote")[0].after(document.querySelector("div#HTML319 .HTML")):document.querySelector("div#HTML319 .HTML").innerHTML=""; 
-document.querySelectorAll(".post-body MoreLink")[0]?document.querySelectorAll(".post-body MoreLink")[0].after(document.querySelector("div#HTML325 .HTML")):document.querySelector("div#HTML325 .HTML").innerHTML="";
---
+++ Inlineads5.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){jQuery('#MBL-Ads').appendTo(jQuery('a[name="more"]'));});
+$(document).ready(function(){jQuery('#HTML339').appendTo(jQuery('.post-body LessonNote'));});
+$(document).ready(function(){jQuery('#HTML335').appendTo(jQuery('.post-body MoreLink'));});
+$(document).ready(function(){jQuery('#HTML348').appendTo(jQuery('.post-body eBook'));});
+$(document).ready(function(){jQuery('#HTML357').appendTo(jQuery('.post-body Note'));});
