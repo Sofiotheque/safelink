33876679e9735b652c5f9d7a943707c11a304157
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2023-04-20 20:14:22 +0100

    Update and rename Inlineads5.js to Inlineads6.js

--- Inlineads6.js
+++ Inlineads6.js
@@ -1,3 +1,5 @@
+function twtparalax(Ad1) {let paralax = document.getElementsByClassName ('twtparalax')[0]; if (paralax) {let Adsp = document.querySelectorAll ('.post-body p')[Ad1 - 1]; Adsp.parentNode .insertBefore (paralax, Adsp.nextSibling)}} twtparalax(2);
+
 $(document).ready(function(){jQuery('#MBL-Ads').appendTo(jQuery('a[name="more"]'));});
 $(document).ready(function(){jQuery('#HTML339').appendTo(jQuery('.post-body LessonNote'));});
 $(document).ready(function(){jQuery('#HTML335').appendTo(jQuery('.post-body MoreLink'));});
